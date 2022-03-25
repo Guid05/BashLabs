71fc4b0d4d42973c5f2b439d53919f39d52064f8
Guid <93153159+Guid05@users.noreply.github.com>
2022-03-25 16:52:34 +0100

    added comments

--- sshsetuid.sh
+++ sshsetuid.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 read -p "Location of list" listlocation
 
+# checks if listlocation file exists and sees if it is a file
 cat $listlocation > /dev/null && listcontent=$(cat $listlocation)
 mkdir ~/setuid 2> /dev/null
 
+# goes through every ip/host provided by list
 for i in $listcontent
 do
+        # if file can be touched without error, ssh: and sends matches to specified to touched file
         touch ~/setuid/$i
         if [[ $? -eq 0 ]]; then
                 ssh $i '(find / -perm -u+s 2> /dev/null)' &> ~/setuid/$i
