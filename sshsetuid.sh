1e903ba76f3a5f1273b4f732af3f163f40ed811e
Guid <93153159+Guid05@users.noreply.github.com>
2022-03-25 16:46:14 +0100

    Update sshsetuid.sh
    Added check to see if file exist

--- sshsetuid.sh
+++ sshsetuid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Location of list" listlocation
 
-listcontent=$(cat $listlocation)
+cat $listlocation > /dev/null && listcontent=$(cat $listlocation)
 mkdir ~/setuid 2> /dev/null
 
 for i in $listcontent
